f231a09b5b7737fc826c40e33417da844a3658b5
Andrey Kiselev <omega-faworit@yandex.ru>
2021-10-31 18:36:43 +0300

    fix: add main fn

--- src/index.ts
+++ src/index.ts
@@ -8,58 +8,62 @@ import {
   replaceVersion,
 } from './utils';
 
-try {
-  const githubToken = getInput('GITHUB_TOKEN', {
-    trimWhitespace: true,
-    required: true,
-  });
-  const assemblyFile = getInput('assembly-file', {
-    trimWhitespace: true,
-    required: true,
-  });
-  const increaseBuild = getBooleanInput('increase-build', {
-    trimWhitespace: true,
-  });
-  const increaseRelease = getBooleanInput('increase-release', {
-    trimWhitespace: true,
-  });
-  const tag = getInput('tag', {
-    trimWhitespace: true,
-  });
+const main = async () => {
+  try {
+    const githubToken = getInput('GITHUB_TOKEN', {
+      trimWhitespace: true,
+      required: true,
+    });
+    const assemblyFile = getInput('assembly-file', {
+      trimWhitespace: true,
+      required: true,
+    });
+    const increaseBuild = getBooleanInput('increase-build', {
+      trimWhitespace: true,
+    });
+    const increaseRelease = getBooleanInput('increase-release', {
+      trimWhitespace: true,
+    });
+    const tag = getInput('tag', {
+      trimWhitespace: true,
+    });
 
-  const file = readFileSync(assemblyFile, { encoding: 'utf8' });
-  const version = findAssemblyVersion(file);
-  const versionInfo = parseVersion(version);
-  if (increaseBuild) {
-    versionInfo.build += 1;
-    versionInfo.tag = undefined;
-  }
-  if (increaseRelease) {
-    versionInfo.release += 1;
-    versionInfo.build = 0;
-    versionInfo.tag = undefined;
-  }
-  if (tag) {
-    versionInfo.tag = tag;
-  }
-  const newVersion = buildVersionString(versionInfo);
-  const newFile = replaceVersion(file, newVersion);
-  writeFileSync(assemblyFile, newFile);
+    const file = readFileSync(assemblyFile, { encoding: 'utf8' });
+    const version = findAssemblyVersion(file);
+    const versionInfo = parseVersion(version);
+    if (increaseBuild) {
+      versionInfo.build += 1;
+      versionInfo.tag = undefined;
+    }
+    if (increaseRelease) {
+      versionInfo.release += 1;
+      versionInfo.build = 0;
+      versionInfo.tag = undefined;
+    }
+    if (tag) {
+      versionInfo.tag = tag;
+    }
+    const newVersion = buildVersionString(versionInfo);
+    const newFile = replaceVersion(file, newVersion);
+    writeFileSync(assemblyFile, newFile);
 
-  if (!process.env.GITHUB_REPOSITORY) {
-    throw new Error('Cannot get Github repository from environment variable');
+    if (!process.env.GITHUB_REPOSITORY) {
+      throw new Error('Cannot get Github repository from environment variable');
+    }
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+    await createCommit({
+      file: {
+        content: newFile,
+        path: assemblyFile,
+      },
+      githubToken,
+      message: `Update version from ${version} to ${newVersion}`,
+      owner,
+      repo,
+    });
+  } catch (error) {
+    setFailed(error as Error);
   }
-  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-  await createCommit({
-    file: {
-      content: newFile,
-      path: assemblyFile,
-    },
-    githubToken,
-    message: `Update version from ${version} to ${newVersion}`,
-    owner,
-    repo,
-  });
-} catch (error) {
-  setFailed(error as Error);
-}
+};
+
+main();
